7d6ff8e1a99d9fbdc49be192b1d440a32f588c11
kanji11 <kanji11@users.noreply.github.com>
2014-09-17 17:13:44 +0530

    Added By Kanji Patel

---
+++ ModGeneral.vb
@@ -0,0 +1,15 @@
+﻿Module ModGeneral
+    Public dsRDC As New DataSet
+    Public dtRDP As New DataTable
+
+    Private m_bWithDrives As Boolean = False
+
+    Public Property WithDrive() As Boolean
+        Get
+            Return m_bWithDrives
+        End Get
+        Set(ByVal value As Boolean)
+            m_bWithDrives = value
+        End Set
+    End Property
+End Module
