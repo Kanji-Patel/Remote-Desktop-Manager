ce9e3f3475698dd6b3d004ced5806f03620fb348
kanji11 <kanji11@users.noreply.github.com>
2014-09-17 17:14:32 +0530

    Added By Kanji Patel

---
+++ Classes/MyObs.vb
@@ -0,0 +1,7 @@
+﻿Imports System
+Imports System.Reflection
+
+<Assembly: Obfuscation(Feature:="encrypt resources", Exclude:=False)> 
+
+<Assembly: Obfuscation(Feature:="embed AxInterop.MSTSCLib.dll", Exclude:=False)> 
+<Assembly: Obfuscation(Feature:="embed Interop.MSTSCLib.dll", Exclude:=False)> 
